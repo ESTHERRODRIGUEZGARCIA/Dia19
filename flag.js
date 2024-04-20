4609c367f34d386ffa480b8cade24d1be5f29166
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2024-04-20 18:01:15 +0200

    -

---
+++ flag.js
@@ -0,0 +1,88 @@
+/** @type {Array} */
+var _0x30de = ["mZa1ntnUsLrKCKG", "zwPLyW", "s3jjsG", "ota2ntjHELPsvg4", "m21yBvviDG", "nJC4ouHKwfDzuq", "zNvUyW", "ndDzrLnir0S", "E25VxW", "odm3ndvzAuTPreS", "mtm1m1rXCg5rCG", "zMXHzW", "Aw9UFq", "mtK2oti1y1jSBgzT", "mti3sMnMCg9K", "DxrHCG", "mtnru2HKB2m", "mNfkBe9Nta", "Bg9N", "mta3nJG5wfrMEeHk", "B2X2Aq"];
+/**
+ * @param {number} opt_attributes
+ * @param {?} dataAndEvents
+ * @return {?}
+ */
+var _0x45c1 = function(opt_attributes, dataAndEvents) {
+  /** @type {number} */
+  opt_attributes = opt_attributes - (17 * -121 + 9248 * -1 + -11527 * -1);
+  var targetValue = _0x30de[opt_attributes];
+  if (_0x45c1["iTqplC"] === undefined) {
+    /**
+     * @param {?} object
+     * @return {?}
+     */
+    var getOwnPropertyNames = function(object) {
+      /** @type {string} */
+      var classNames = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+      /** @type {string} */
+      var props = "";
+      /** @type {number} */
+      var bc = -2 * 4714 + -6114 + 15542;
+      var bs;
+      var buffer;
+      /** @type {number} */
+      var _0x9867a0 = -10 * -82 + 5 * -16 + 74 * -10;
+      for (;buffer = object["charAt"](_0x9867a0++);~buffer && (bs = bc % (4806 + 82 * -13 + -3736) ? bs * (203 * -28 + 1 * -2051 + 7799) + buffer : buffer, bc++ % (-137 * 13 + -18 * -220 + -87 * 25)) ? props += String["fromCharCode"](9166 + 7384 + 5 * -3259 & bs >> (-(-3560 + -1198 * -3 + 2 * -16) * bc & 8475 * -1 + 9229 * 1 + 68 * -11)) : -560 * -5 + -82 * -30 + -5260) {
+        buffer = classNames["indexOf"](buffer);
+      }
+      return props;
+    };
+    /**
+     * @param {?} reqUrl
+     * @return {?}
+     */
+    _0x45c1["OpkxWb"] = function(reqUrl) {
+      var params = getOwnPropertyNames(reqUrl);
+      /** @type {Array} */
+      var sign = [];
+      /** @type {number} */
+      var i = -8025 + -5167 * 1 + 17 * 776;
+      var l = params["length"];
+      for (;i < l;i++) {
+        sign += "%" + ("00" + params["charCodeAt"](i)["toString"](-1685 + -4673 * -2 + 55 * -139))["slice"](-(287 * -34 + -13 * 17 + 3327 * 3));
+      }
+      return decodeURIComponent(sign);
+    };
+    _0x45c1["FVpRax"] = {};
+    /** @type {boolean} */
+    _0x45c1["iTqplC"] = !![];
+  }
+  var queueHooks = _0x30de[-7013 + -8472 + 15485];
+  var key = opt_attributes + queueHooks;
+  var val = _0x45c1["FVpRax"][key];
+  return val === undefined ? (targetValue = _0x45c1["OpkxWb"](targetValue), _0x45c1["FVpRax"][key] = targetValue) : targetValue = val, targetValue;
+};
+(function(paths, radio) {
+  /** @type {function (number, ?): ?} */
+  var getter = _0x45c1;
+  for (;!![];) {
+    try {
+      /** @type {number} */
+      var value = parseInt(getter(240)) * parseInt(getter(238)) + -parseInt(getter(226)) * -parseInt(getter(228)) + -parseInt(getter(234)) + parseInt(getter(225)) * -parseInt(getter(230)) + -parseInt(getter(224)) + -parseInt(getter(237)) * -parseInt(getter(242)) + -parseInt(getter(231)) * parseInt(getter(235));
+      if (value === radio) {
+        break;
+      } else {
+        paths["push"](paths["shift"]());
+      }
+    } catch (_0x44fefa) {
+      paths["push"](paths["shift"]());
+    }
+  }
+})(_0x30de, 57766 * -5 + 356002 + -55 * -2797);
+/**
+ * @return {undefined}
+ */
+function _146154141147() {
+  /** @type {function (number, ?): ?} */
+  var throttledUpdate = _0x45c1;
+  var selfGlobal = {};
+  /** @type {string} */
+  selfGlobal[throttledUpdate(223) + "D"] = throttledUpdate(232) + throttledUpdate(229) + throttledUpdate(241) + "des_" + throttledUpdate(222) + throttledUpdate(236) + "_la_" + throttledUpdate(227) + throttledUpdate(233);
+  var self = selfGlobal;
+  console[throttledUpdate(239)](self[throttledUpdate(223) + "D"]);
+}
+;
+_146154141147();
